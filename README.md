fc42502b6b3571aaf9a7a5fda32b0eb4df58ce45
kevdaws <krdaws00@gmail.com>
2021-04-15 14:31:20 -0400

    setup discord bot client

---
+++ README.md
@@ -0,0 +1,3 @@
+# Discord Bot Testing
+
+- Bot Token stored in .env file.
---
+++ src/index.js
@@ -0,0 +1,27 @@
+// require the discord.js module
+const Discord = require('discord.js');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+// create a new Discord client
+const client = new Discord.Client();
+
+// when the client is ready, run this code.
+// this event will only trigger one time after logging in.
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// login to Discord with bot token from env.
+const BOT_TOKEN = process.env.BOT_TOKEN;
+client.login(BOT_TOKEN);
+
+client.on('message', message => {
+	
+    if (message.channel.id === '781384872773812308') {
+        if (message.content === 'sheesh' || message.content === "!sheesh") {
+            message.channel.send('SHEEEEEEEEEEEEEEEEEEEEEEEEEEEESH');
+        }
+    }
+});
