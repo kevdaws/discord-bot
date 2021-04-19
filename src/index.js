cf811236727b1f09632aca349b402ebcdc2a6463
kevdaws <krdaws00@gmail.com>
2021-04-18 20:41:34 -0400

    got random gif id

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const fetch = require("node-fetch");
+
 // require the discord.js module
 const Discord = require('discord.js');
 
@@ -6,6 +8,8 @@ dotenv.config();
 
 // create a new Discord client
 const client = new Discord.Client();
+
+const GIPHY_TOKEN = process.env.GIPHY_TOKEN;
 
 // when the client is ready, run this code.
 // this event will only trigger one time after logging in.
@@ -20,11 +24,29 @@ client.login(BOT_TOKEN);
 // get env variable for bot testing channel.
 const BOT_CHANNEL = process.env.BOT_CHANNEL;
 
+const gif = async () => {
+    const url = (`https://api.giphy.com/v1/randomid?api_key=${GIPHY_TOKEN}`);
+    const response = await fetch(url);
+    const resJSON = await response.json();
+    //console.log(resJSON);
+    const random_id = resJSON.data.random_id;
+    console.log(`${url}/gifs/${random_id}`);
+    const image = await fetch(`${url}/gifs/${random_id}`);
+    const imageJSON = await image.json();
+    console.log(imageJSON);
+}
+
+
+
+console.log("yo");
+gif();
+
 client.on('message', message => {
-	
+    
     if (message.channel.id === BOT_CHANNEL) {
-        if (message.content === 'sheesh' || message.content === "!sheesh") {
-            message.channel.send('SHEEEEEEEEEEEEEEEEEEEEEEEEEEEESH');
+        if (message.content === '!gif' || message.content === "!gif") {
+            gif();
         }
     }
-});
+});   
+
